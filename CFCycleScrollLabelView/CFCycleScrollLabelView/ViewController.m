5292e7b22501df95d0853c26c0a48c91e66bec84
Peak <545486205@qq.com>
2017-02-23 14:14:27 +0800

    初始化项目

---
+++ CFCycleScrollLabelView/CFCycleScrollLabelView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CFCycleScrollLabelView
+//
+//  Created by Peak on 17/2/23.
+//  Copyright © 2017年 陈峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CFCycleScrollLabelView/CFCycleScrollLabelView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CFCycleScrollLabelView
+//
+//  Created by Peak on 17/2/23.
+//  Copyright © 2017年 陈峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
